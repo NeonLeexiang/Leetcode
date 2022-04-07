8662cb763762c772031692b05530c39a9143532e
NeonLeexiang <neonleexiang@outlook.com>
2022-04-07 22:14:06 +0800

    这两题都没有做出来

---
+++ 7.整数反转.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x) {
+            // be sure for the boundary
+            if (rev < INT_MIN / 10 || rev > INT_MAX / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+        return rev;
+    }
+};
+// @lc code=end
+
