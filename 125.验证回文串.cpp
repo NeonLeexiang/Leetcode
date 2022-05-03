8c36bc18a4258ea04af2c9569faaf66beabab2a0
NeonLeexiang <neonleexiang@outlook.com>
2022-05-03 14:57:12 +0800

    快慢指针的思路也可以解决类似的字符串问题，相同的，字符串，数组，链表都具有线性性，都可以通过快慢指针的方式解决问题

---
+++ 125.验证回文串.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=cpp
+ *
+ * [125] 验证回文串
+ */
+
+// @lc code=start
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int len = s.length();
+//         int i = 0;
+//         int j = len - 1;
+//         while (i < j) {
+//             if (!(('A'<=s[i] && s[i]<='Z') || ('a'<=s[i] && s[i]<='z') || ('0'<=s[i] && s[i]<='9'))) {
+//                 i++;
+//                 continue;
+//             }
+//             if (!(('A'<=s[j] && s[j]<='Z') || ('a'<=s[j] && s[j]<='z')|| ('0'<=s[j] && s[j]<='9'))) {
+//                 j--;
+//                 continue;
+//             }
+//             int asc_i = int(s[i]);
+//             int asc_j = int(s[j]);
+
+//             if (asc_i - int('a') >= 0)
+//                 asc_i = int('A') + asc_i - int('a');
+//             if (asc_j - int('a') >= 0)
+//                 asc_j = int('A') + asc_j - int('a');
+            
+//             cout<<asc_i<<endl;
+//             cout<<asc_j<<endl;
+
+//             if (asc_i != asc_j)
+//                 return false;
+//             i++; j--;
+//         }
+//         return true;
+
+//     }
+// };
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) {
+                ++left;
+            }
+            while (left < right && !isalnum(s[right])) {
+                --right;
+            }
+            if (left < right) {
+                if (tolower(s[left]) != tolower(s[right])) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
