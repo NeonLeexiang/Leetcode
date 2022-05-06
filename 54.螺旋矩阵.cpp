3669528abc4946c01fc8b2fc7fbdda86ae4b5f31
NeonLeexiang <neonleexiang@outlook.com>
2022-05-06 15:02:08 +0800

    二维矩阵花式遍历

---
+++ 54.螺旋矩阵.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=cpp
+ *
+ * [54] 螺旋矩阵
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int upper_bound = 0;
+        int lower_bound = m-1;
+        int left_bound = 0;
+        int right_bound = n-1;
+
+        vector<int> res;    // 注意这里不能初始化，得利用 push_back 方法
+
+        while (res.size() < m * n) {
+            if (upper_bound <= lower_bound) {
+                // 在顶部从左向右遍历
+                for (int j = left_bound; j <= right_bound; j++) {
+                    res.push_back(matrix[upper_bound][j]);
+                }
+                // 上边界下移
+                upper_bound++;
+            }
+
+            if (left_bound <= right_bound) {
+                // 在右侧从上向下遍历
+                for (int i = upper_bound; i <= lower_bound; i++) {
+                    res.push_back(matrix[i][right_bound]);
+                }
+                // 右边界左移
+                right_bound--;
+            }
+
+            if (upper_bound <= lower_bound) {
+                // 在底部从右向左遍历
+                for (int j = right_bound; j >= left_bound; j--) {
+                    res.push_back(matrix[lower_bound][j]);
+                }
+                // 下边界上移
+                lower_bound--;
+            }
+
+            if (left_bound <= right_bound) {
+                // 在左侧从下向上遍历
+                for (int i = lower_bound; i >= upper_bound; i--) {
+                    res.push_back(matrix[i][left_bound]);
+                }
+                // 左边界右移
+                left_bound++;
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
