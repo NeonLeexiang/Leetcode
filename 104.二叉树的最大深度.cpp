10203851dc010592e249b56754d540e43422c7c2
NeonLeexiang <neonleexiang@outlook.com>
2022-05-21 13:19:12 +0800

    开启二叉树刷题模式

--- 104.二叉树的最大深度.cpp
+++ 104.二叉树的最大深度.cpp
@@ -41,6 +41,20 @@ public:
         // 后序位置重制 depth
         depth--;
     }
+    // // 定义：输入根节点，返回这棵二叉树的最大深度
+    // int maxDepth(TreeNode root) {
+    //     if (root == null) {
+    //         return 0;
+    //     }
+    //     // 利用定义，计算左右子树的最大深度
+    //     int leftMax = maxDepth(root.left);
+    //     int rightMax = maxDepth(root.right);
+    //     // 整棵树的最大深度等于左右子树的最大深度取最大值，
+    //     // 然后再加上根节点自己
+    //     int res = Math.max(leftMax, rightMax) + 1;
+
+    //     return res;
+    // }
 
 };
 // @lc code=end
